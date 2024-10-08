b60eadc9c8b9e0cff6b29b85c66d4949accea689
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-10-08 23:13:04 +0530

    Program

---
+++ Programming_In_Cpp/if_else.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int num ;
+
+    cout<<"Enter any number: "<<endl;
+    cin>>num;
+
+    if(num%2==0)
+    {
+        cout<<"it is even number";
+    }
+    else
+    {
+        cout<<"it is odd number";
+    }
+    return 0;
+}
+
+
