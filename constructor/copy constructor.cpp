861a8b01e1717093f1e200cc0e95be2b87c021c9
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-11-01 21:37:20 +0530

    Copy Constructor

---
+++ constructor/copy constructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class sample
+{
+    int id;
+public:
+    void init (int x)
+    {
+        id =x;
+    }
+    void display()
+    {
+        cout<<endl<<"ID="<<id;
+    }
+};
+int main()
+{
+    sample obj1;
+    obj1.init(50);
+    obj1.display();
+    sample obj2(obj1);
+    obj2.display();
+    return 0;
+}
