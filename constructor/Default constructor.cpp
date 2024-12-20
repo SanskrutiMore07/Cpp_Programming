3fe1c2d4f888aa7b178875f47073bcd922b370fd
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-12-20 20:16:31 +0530

    program

---
+++ constructor/Default constructor.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class constant
+{
+public:
+    construct()
+    {
+        a=10;
+        b=20;
+    }
+};
+int main()
+{
+  construct c;
+  cout<<"a:"<<c.a<<endl;
+  cout<<"b:"<<c.b<<endl;
+  return 0;
+}
